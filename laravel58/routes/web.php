c0757977c619b30bb0c7b05f3c01a54f06509d31
albertmxx <1160445224@qq.com>
2022-02-24 17:47:28 +0800

    laravel learn add files

--- laravel58/routes/web.php
+++ laravel58/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::view('/home2', 'home', [1, 2, 3]);
+
+Route::get('test', '\App\Http\Controllers\TttController@index');
