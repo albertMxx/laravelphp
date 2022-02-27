8f3a933f748a51d25eabbad0fa1b99bf2f950bfb
albertMao <1160445224@qq.com>
2022-02-27 14:03:48 +0800

    web develop

---
+++ laravel58/resources/views/index.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>仌</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="personal project --albertMao"/>
+    <meta name="keywords" content="albertMao"/>
+    <meta name="author" content="albertMao"/>
+
+    <!-- Animate -->
+    <link rel="stylesheet" href="/css/animate.css">
+    <!-- Icomoon -->
+    <link rel="stylesheet" href="/css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<div id="fh5co-offcanvas">
+    <a href="#" class="fh5co-close-offcanvas js-fh5co-close-offcanvas"><span><i
+                    class="icon-cross3"></i> <span>Close</span></span></a>
+    <div class="fh5co-bio">
+        <h2>Hi friend!</h2>
+        <p>一个小码农的生活工作小工具<br> -- albertMao</p>
+    </div>
+
+    <div class="fh5co-menu">
+        <div class="fh5co-box">
+            <ul>
+                <li><a href="index.php/user"><i class="icon-user"></i></a></li>
+                <li><a href="#">博客</a></li>
+                <li><a href="#">个人账单</a></li>
+                <li><a href="#">隐私日记</a></li>
+                <li><a href="#"><i class="icon-log-out"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- END #fh5co-offcanvas -->
+<header id="fh5co-header">
+    <div class="container-fluid">
+        <div class="col-lg-12 col-md-12 text-center">
+            <h1>仌<sup>
+                    <small>（努力向上吧）</small>
+                </sup></h1>
+        </div>
+        <div class="row">
+            <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+        </div>
+    </div>
+</header>
+<!-- END #fh5co-header -->
+<div class="container-fluid">
+    <div class="row fh5co-post-entry">
+        <article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+            <h2 class="fh5co-article-title"><a href="single.html">We Eat and Drink All Night</a></h2>
+            <!--<span class="fh5co-meta"><a href="single.html">Food &amp; Drink</a></span>-->
+            <span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+            {{--<figure>--}}
+            {{--<a href="single.html"><img src="images/pic_1.jpg" alt="Image" class="img-responsive"></a>--}}
+            {{--</figure>--}}
+            <div class="fh5co-highlight right">
+                <h4>Highlight</h4>
+                <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A
+                    small river named Duden flows by their place and supplies it with the necessary regelialia</p>
+            </div>
+        </article>
+    </div>
+</div>
+
+<footer id="fh5co-footer">
+    <p>
+        <small>&copy; 2022-albertMao</small>
+    </p>
+</footer>
+
+
+<!-- jQuery -->
+<script src="/js/jquery.min.js"></script>
+<!-- jQuery Easing -->
+<script src="/js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="/js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="/js/jquery.waypoints.min.js"></script>
+<!-- Main JS -->
+<script src="/js/main.js"></script>
+<!-- Modernizr JS -->
+<script src="/js/modernizr-2.6.2.min.js"></script>
+<!-- FOR IE9 below -->
+<!--[if lt IE 9]>-->
+<script src="/js/respond.min.js"></script>
+
+</body>
+</html>
+
---
+++ laravel58/resources/views/user.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>仌</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="personal project --albertMao"/>
+    <meta name="keywords" content="albertMao"/>
+    <meta name="author" content="albertMao"/>
+
+    <!-- Animate -->
+    <link rel="stylesheet" href="/css/animate.css">
+    <!-- Icomoon -->
+    <link rel="stylesheet" href="/css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/style.css">
+
+    <style>
+        .fh5co-post-prev {
+            min-width: 15%;
+            left: 18%;
+            margin-left: -25px;
+            margin-top: 10px;
+        }
+
+        .fh5co-post-prev:hover {
+            left: 18%;
+            margin-left: 10px;
+            color: #000;
+        }
+
+        .fh5co-post-next {
+            min-width: 15%;
+            right: 18%;
+            margin-right: -25px;
+            margin-top: 10px;
+        }
+
+        .fh5co-post-next:hover {
+            right: 18%;
+            margin-right: 10px;
+            color: #000;
+        }
+
+    </style>
+</head>
+<body>
+
+<header id="fh5co-header">
+    <div class="container-fluid">
+        <a href="/"><i class="icon-back"></i></a>
+        <div class="row text-center">
+            <b id="fh5co-logo"><i class="icon-user"></i></b>
+        </div>
+    </div>
+
+</header>
+<!-- END #fh5co-header -->
+<div class="container-fluid">
+    <div class="row fh5co-post-entry single-entry">
+        <article
+                class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-6 col-sm-offset-4 col-xs-12 col-xs-offset-0">
+            <figure class="animate-box col-md-8 col-md-offset-2">
+                <span class="fh5co-meta">昵称</span>
+                <input type="text" id="username" name="username" class="form-control" placeholder="昵称">
+            </figure>
+
+            <figure class="animate-box col-md-8 col-md-offset-2">
+                <span class="fh5co-meta">密码</span>
+                <input type="password" id="password" name="password" class="form-control" placeholder="密码">
+            </figure>
+
+            <figure class="animate-box col-md-4 col-md-offset-4">
+                <span class="fh5co-meta">性别</span>
+                <select type="text" name="sex" id="sex" class="form-control">
+                    <option>保密</option>
+                    <option>男</option>
+                    <option>女</option>
+                </select>
+            </figure>
+
+            <figure class="col-md-4 col-md-offset-4">
+                <a href="#" class="fh5co-post-prev"><span><i class="icon-login"></i>登录</span></a>
+                <a href="#" class="fh5co-post-next"><span>注册<i class="icon-add-user"></i></span></a>
+            </figure>
+
+        </article>
+    </div>
+</div>
+
+<!-- jQuery -->
+<script src="/js/jquery.min.js"></script>
+<!-- jQuery Easing -->
+<script src="/js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="/js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="/js/jquery.waypoints.min.js"></script>
+<!-- Main JS -->
+<script src="/js/main.js"></script>
+<!-- Modernizr JS -->
+<script src="/js/modernizr-2.6.2.min.js"></script>
+<!-- FOR IE9 below -->
+<!--[if lt IE 9]>-->
+<script src="/js/respond.min.js"></script>
+
+</body>
+</html>
+
